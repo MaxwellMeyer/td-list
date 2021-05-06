8fc647cc1b49f2fbc1751d681ff0626318019e66
Maxwell Meyer <maxreadswell@gmail.com>
2021-05-05 21:42:40 -0700

    remove loops, replace Dispose method

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -8,6 +8,7 @@ namespace ToDoList.Tests
   [TestClass]
   public class ItemTests : IDisposable
   {
+
     public void Dispose()
     {
       Item.ClearAll();
@@ -16,16 +17,21 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item("test"); //We pass in "test" as an argument here.
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
 
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
+      //Arrange
       string description = "Walk the dog.";
+
+      //Act
       Item newItem = new Item(description);
       string result = newItem.Description;
+
+      //Assert
       Assert.AreEqual(description, result);
     }
 
@@ -48,33 +54,31 @@ namespace ToDoList.Tests
     [TestMethod]
     public void GetAll_ReturnsEmptyList_ItemList()
     {
-  
+      // Arrange
       List<Item> newList = new List<Item> { };
 
- 
+      // Act
       List<Item> result = Item.GetAll();
 
-  
+      // Assert
       CollectionAssert.AreEqual(newList, result);
     }
 
     [TestMethod]
-public void GetAll_ReturnsItems_ItemList()
-{
-  //Arrange
-  string description01 = "Walk the dog";
-  string description02 = "Wash the dishes";
-  Item newItem1 = new Item(description01);
-  Item newItem2 = new Item(description02);
-  List<Item> newList = new List<Item> { newItem1, newItem2 };
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
 
-  //Act
-  List<Item> result = Item.GetAll();
+      //Act
+      List<Item> result = Item.GetAll();
 
-  //Assert
-  CollectionAssert.AreEqual(newList, result);
-}
-
-
-      }
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
     }
+  }
+}
