2df186b9f458af6ff6f3249982368dce981a692e
Maxwell Meyer <maxreadswell@gmail.com>
2021-05-16 20:08:42 -0700

    begin to refactor Item model

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace ToDoList.Models
 {
@@ -6,13 +7,11 @@ namespace ToDoList.Models
   {
     public string Description { get; set; }
     public int Id { get; }
-    private static List<Item> _instances = new List<Item> { };
+
 
     public Item(string description)
     {
       Description = description;
-      _instances.Add(this);
-      Id = _instances.Count;
     }
 
     public static List<Item> GetAll()
@@ -22,12 +21,12 @@ namespace ToDoList.Models
 
     public static void ClearAll()
     {
-      _instances.Clear();
+
     }
 
     public static Item Find(int searchId)
     {
-      return _instances[searchId - 1];
+      return [searchId - 1];
     }
   }
 }
