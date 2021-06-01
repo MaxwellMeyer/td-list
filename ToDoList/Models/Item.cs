2d2e628e8dfbf49f29b7ea6fbc6e2e1fbcb25f6c
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-01 13:14:54 -0700

    update item model

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,12 +6,13 @@ namespace ToDoList.Models
   {
     public Item()
     {
-      this.JoinEntities = new HashSet<CategoryItem>();
+      this.Categories = new HashSet<CategoryItem>();
     }
 
     public int ItemId { get; set; }
     public string Description { get; set; }
+    public virtual ApplicationUser User { get; set; }
 
-    public virtual ICollection<CategoryItem> JoinEntities { get; }
+    public ICollection<CategoryItem> Categories { get; }
   }
 }
